efba9e2326cc12d5258da392d0ec62a9e05eaad5
Moaaz Abdo <moaaz.abdo010150@gmail.com>
2020-11-23 14:35:13 +0200

    updated we add sign in page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
 
         <Route exact  path="/" component={HomePage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
+        <Route path="/signin" component={SignInSignUpPage}></Route>
 
       </Switch>
 
