992d0248ca2c39d953050e53fab9f78a87c29da0
Moaaz Abdo <moaaz.abdo010150@gmail.com>
2020-11-23 21:00:50 +0200

    make sign in

--- src/App.js
+++ src/App.js
@@ -5,23 +5,57 @@ import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
+class App extends React.Component {
+  constructor() {
+    super()
+  
+    this.state = {
+       currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+
+    /**
+     * onAuthStateChanged(). it is a message system between application and firebase.
+     * whenever anychanges occurs on firebase from any source related to this application
+     * firebase sends out that message that says state changed the user has updated.
+     * and this connection is always open as long as our application component is mounted.
+     * because it'a an open subscrition we also have to close subscription when this unmount because we don't 
+     * any memory leak in our javascript application . 
+     */
       
-      <Header />
-            
-      <Switch>
 
-        <Route exact  path="/" component={HomePage}></Route>
-        <Route path="/shop" component={ShopPage}></Route>
-        <Route path="/signin" component={SignInSignUpPage}></Route>
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    });
+  }
+  
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
 
-      </Switch>
-
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <Header />
+              
+        <Switch>
+  
+          <Route exact  path="/" component={HomePage}></Route>
+          <Route path="/shop" component={ShopPage}></Route>
+          <Route path="/signin" component={SignInSignUpPage}></Route>
+  
+        </Switch>
+  
+      </div>
+    );
+  }
+  
 }
 
 export default App;
